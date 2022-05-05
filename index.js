ed188cc50141e21e749af8c304af288a6b300d39
Lido Lee <Lido_Lee@asus.com>
2022-05-05 23:16:06 +0800

    install cors to fix localhost can't get post request.

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const Serie = require("./models/serie");
 const keccak256 = require("keccak256");
 const { MerkleTree } = require("merkletreejs");
 const keyList = require("./series.json");
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
 //MerkleProof function
 function MerkleProof(series) {
@@ -22,6 +28,7 @@ function MerkleProof(series) {
 }
 
 //Middleware
+app.use(cors(corsOptions));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true })); // 處理get,post,... request 取得 req.body
