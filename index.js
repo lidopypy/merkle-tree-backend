e4f15b0ff434d195f525b67eced009235831506e
Lido Lee <Lido_Lee@asus.com>
2022-05-05 16:06:00 +0800

    modify app.listen

--- index.js
+++ index.js
@@ -63,6 +63,6 @@ app.get("/*", (req, res) => {
   res.send("Not allowed.");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000.");
 });
